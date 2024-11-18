8ea227b510fc5eb9652d48abd895e11c169be6b1
Suhelkhan12 <suhel7822@gmail.com>
2024-11-18 14:16:11 +0530

    video details fetched and fed to the slider

--- app/condense/_components/video-details.tsx
+++ app/condense/_components/video-details.tsx
@@ -35,38 +35,46 @@ const VideoInputDetails = ({ videoFile, onClear }: VideoInputDetailsProps) => {
   });
 
   return (
-    <Card className=" flex-grow bg-secondary">
-      <CardHeader className="pb-2">
-        <CardTitle className="flex justify-between items-center text-xl">
-          Input File
-          <Button onClick={onClear}>Change video</Button>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription className="mb-4">
-          <div className="flex flex-col gap-2">
-            <div className="flex items-center gap-4">
-              <span className=" w-20">File name</span>
-              <p className="font-semibold text-primary">{videoFile.fileName}</p>
+    <div className="flex-grow flex gap-4 flex-col">
+      <Card className="bg-secondary">
+        <CardHeader className="pb-2">
+          <CardTitle className="flex justify-between items-center text-xl">
+            Input File
+            <Button onClick={onClear}>Change video</Button>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="mb-4">
+            <div className="flex flex-col gap-2">
+              <div className="flex items-center gap-4">
+                <span className=" w-20">File name</span>
+                <p className="font-semibold text-primary">
+                  {videoFile.fileName}
+                </p>
+              </div>
+              <div className="flex items-center gap-4">
+                <span className="w-20">File size</span>
+                <p className="font-semibold text-primary">
+                  {byteConversion(videoFile.fileSize)}
+                </p>
+              </div>
             </div>
-            <div className="flex items-center gap-4">
-              <span className="w-20">File size</span>
-              <p className="font-semibold text-primary">
-                {byteConversion(videoFile.fileSize)}
-              </p>
-            </div>
-            <div>
-              <span className="w-20 font-medium text-primary ">Trim video</span>
-              <VideoTrim
-                videoSettings={videoSettings}
-                disabled={false}
-                onVideoSettingChange={() => {}}
-              />
-            </div>
-          </div>
-        </CardDescription>
-      </CardContent>
-    </Card>
+          </CardDescription>
+        </CardContent>
+      </Card>
+      <Card className="bg-secondary">
+        <CardHeader className="pb-2">
+          <CardTitle>Trim video</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <VideoTrim
+            videoSettings={videoSettings}
+            disabled={false}
+            onVideoSettingChange={setVideoSettings}
+          />
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
--- app/condense/_components/video-trim.tsx
+++ app/condense/_components/video-trim.tsx
@@ -1,5 +1,7 @@
 import type { VideoInpSettings } from "@/utils/types";
 import { Slider } from "@/components/ui/slider";
+import { useEffect, useState } from "react";
+import { calculateTimeInHoursMinutesSeconds } from "@/utils/format";
 
 type VideoTrimProps = {
   videoSettings: VideoInpSettings;
@@ -12,5 +14,61 @@ export const VideoTrim = ({
   onVideoSettingChange,
   disabled,
 }: VideoTrimProps) => {
-  return <Slider className="mt-2" />;
+  const [videoEndTime, setVideoEndTime] = useState(0);
+  const { customEndTime, customStartTime } = videoSettings;
+  const startTime = calculateTimeInHoursMinutesSeconds(customStartTime);
+  const endTime = calculateTimeInHoursMinutesSeconds(customEndTime);
+
+  useEffect(() => {
+    const video = document.getElementById(
+      "condense-video-id"
+    ) as HTMLVideoElement;
+
+    if (video) {
+      const handleLoadedMetaData = () => {
+        const videoDurationInSeconds = video.duration;
+        onVideoSettingChange({
+          ...videoSettings,
+          customEndTime: videoDurationInSeconds,
+        });
+        setVideoEndTime(videoDurationInSeconds);
+      };
+
+      video.addEventListener("loadedmetadata", handleLoadedMetaData);
+      return () => {
+        if (video) {
+          video.removeEventListener("loadedmetadata", handleLoadedMetaData);
+        }
+      };
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col gap-4">
+      <Slider
+        className="mt-2"
+        value={[customStartTime, customEndTime]}
+        disabled={disabled}
+        max={videoEndTime}
+        step={1}
+        onValueChange={(val: number[]) => {
+          const [startTime, endTime] = val;
+          onVideoSettingChange({
+            ...videoSettings,
+            customEndTime: endTime,
+            customStartTime: startTime,
+          });
+        }}
+      />
+      <div className="flex items-center justify-between  text-xs">
+        <p className="flex flex-col gap-1">
+          Start Time:{" "}
+          <span className="text-primary font-medium">{startTime}</span>
+        </p>
+        <p className="flex flex-col gap-1">
+          End Time: <span className=" text-primary font-medium">{endTime}</span>
+        </p>
+      </div>
+    </div>
+  );
 };
